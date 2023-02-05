c81ebb1996671ce8d17d3746c86497180b2f4fe4
Scott McKeefer <Scottzapturk19@gmail.com>
2023-02-05 17:48:14 -0600

    add ball

--- Godot/Objects/player.gd
+++ Godot/Objects/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 150.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
