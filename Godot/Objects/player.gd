928636e651bbfab96c41aede18602b113c89b53e
Scott McKeefer <Scottzapturk19@gmail.com>
2023-02-04 19:57:34 -0600

    Woking on new player for break out noom

--- Godot/Objects/player.gd
+++ Godot/Objects/player.gd
@@ -1,9 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
 
-func _ready():
-	pass
+const SPEED = 300.0
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
