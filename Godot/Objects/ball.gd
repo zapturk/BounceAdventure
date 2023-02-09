7ebb46227bae494fa941f74c41f5a85058f685c1
Scott McKeefer <Scottzapturk19@gmail.com>
2023-02-08 22:33:17 -0600

    new hud and losts of game fixes

--- Godot/Objects/ball.gd
+++ Godot/Objects/ball.gd
@@ -3,25 +3,31 @@ extends CharacterBody2D
 var speed: int = 75
 var go: bool = false
 var player
+var controller
 
 func _ready():
 	randomize()
 	velocity.x = [-.5,.5][randi() % 2]
 	velocity.y = 1
 	player = get_parent().get_node("Player")
-	emit_signal("BallCreated")
+	get_parent().ballCreated()
 
 func _physics_process(delta):
 	if(go):
 		var collision_object = move_and_collide(velocity * speed * delta)
 		if collision_object:
-			if collision_object.get_collider().name == "Player":
+			var colName = collision_object.get_collider().name
+			if colName == "WallBottom":
+				#kill ball if falls out
+				get_parent().ballKilled()
+				queue_free()
+			elif colName == "Player":
 #				# so this make the ball go left if it lands on the left side of the bat or vice versa
 				velocity = velocity.from_angle(Vector2(player.position.x, player.position.y).angle_to_point(Vector2(position.x, position.y)))
+				speed += 10
 			else:
 				# build in bounce for any thing else
 				velocity = velocity.bounce(collision_object.get_normal())
-				#need to kill ball if falls out
 				#need to add brick logic
 	else:
 		position.x = player.position.x
--- Godot/Rooms/test_breakout_room.gd
+++ Godot/Rooms/test_breakout_room.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
 var ballCount: int = 0
-var ball = preload("res://Objects/ball.tscn").instantiate()
+var ball = preload("res://Objects/ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("BallCreated", ballCreate)
-	add_child(ball)
+	createBall()
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(ballCount)
+	if ballCount == 0:
+		createBall()
 	pass
+	
+func createBall():
+	add_child(ball.instantiate())
+	print("ball created")
 
-func ballCreate():
+func ballCreated():
 	ballCount += 1
+	
+func ballKilled():
+	ballCount -= 1
