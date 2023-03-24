6c84421f581dcb5c9fe2da952caa45887bb22567
Scott McKeefer <Scottzapturk19@gmail.com>
2023-03-23 20:06:59 -0500

    Make the Powerup an Area2d
    made this change because it fixes side hit by player

--- Godot/Objects/power_up.gd
+++ Godot/Objects/power_up.gd
@@ -1,8 +1,9 @@
-extends CharacterBody2D
+extends Area2D
 
 const fallSpeed: int = 55 
 const powerUpOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var selectedPowerUp
+var velocity
 @onready var room = get_parent()
 
 func _ready():
@@ -13,12 +14,13 @@ func _ready():
 	
 	$AnimationPlayer.play(selectedPowerUp)
 	velocity = Vector2.DOWN
+	
 
 func _physics_process(delta):
-	var collisionObj = move_and_collide(velocity * fallSpeed * delta)
-	if collisionObj:
-			var objHit = collisionObj.get_collider()
-			match objHit.get_collision_layer():
+	position += velocity * fallSpeed * delta
+
+func _on_body_entered(body):
+	match body.get_collision_layer():
 				Globals.outOfBoundsLayer:
 					queue_free()
 				Globals.playerLayer:
